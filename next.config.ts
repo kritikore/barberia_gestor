9bfea17dde20286c7f60741fec5a8ab88e01cc0a
Julio Orozco (2021600993) - QA/Tester <julio.ao@gmail.com>
2025-12-15 12:25:46 -0600

    fix: correctly disable eslint during build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  /* config options here */
-  /*reactStrictMode: true,
-};*/
-
-eslint: {
+  eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
 export default nextConfig;
